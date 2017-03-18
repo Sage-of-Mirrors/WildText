f3e09e6161b9a5f3385583d6047678db440d9e38
Dylan Ascencio <Dylan Ascencio>
2017-03-18 01:54:16 -0500

    Did a ton of stuff, such as almost finishing loading and setting up the UI.

---
+++ WildText/src/ViewModel/Importing.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GameFormatReader.Common;
+using System.ComponentModel;
+using Microsoft.Win32;
+using System.IO;
+
+namespace WildText.src.ViewModel
+{
+    partial class ViewModel : INotifyPropertyChanged
+    {
+        public void OpenFromDialog()
+        {
+            OpenFileDialog dia = new OpenFileDialog();
+            dia.Filter = "MSBT Files|*.msbt|All Files|*";
+            dia.Title = "Open an MSBT File";
+
+            if (dia.ShowDialog() != true)
+                return;
+
+            WindowTitle = dia.SafeFileName;
+
+            using (FileStream stream = new FileStream(dia.FileName, FileMode.Open, FileAccess.Read))
+            {
+                EndianBinaryReader reader = new EndianBinaryReader(stream, Endian.Big);
+
+                MessageManager = new MessageManager(reader);
+            }
+
+            SelectedMessage = MessageManager.Messages[0];
+        }
+    }
+}
